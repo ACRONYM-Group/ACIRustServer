6634715bcda266e0206f71b35b991682fef9cf58
CarterTS <carterplasek@gmail.com>
2020-12-20 23:34:21 -0600

    Update version

--- src/version.rs
+++ src/version.rs
@@ -1,2 +1,2 @@
-pub static BUILD_VERSION: &str = "2020.12.19.3a";
+pub static BUILD_VERSION: &str = "2020.12.20.1a";
 pub static COMPATIBLE_VERSIONS: &[&str] = &["2020.07.01.1", "dev2020.12.06.1", "dev2020.12.08.1"];
