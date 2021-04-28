6fabab93f1a3bafa7a7d6cf36142e2bdb9418b2c
CarterTS <carterplasek@gmail.com>
2021-04-27 21:28:29 -0500

    Version Increment

--- src/version.rs
+++ src/version.rs
@@ -1,2 +1,2 @@
-pub static BUILD_VERSION: &str = "2020.12.20.1a";
-pub static COMPATIBLE_VERSIONS: &[&str] = &["2020.07.01.1", "dev2020.12.06.1", "dev2020.12.08.1"];
+pub static BUILD_VERSION: &str = "dev2021.4.27.1";
+pub static COMPATIBLE_VERSIONS: &[&str] = &["2020.07.01.1", "dev2020.12.06.1", "dev2020.12.08.1", "2020.12.20.1a"];
